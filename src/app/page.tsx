628072b2f5d6de10b1688e97d01a433c4840acf0
Jannis Heydemann <heydemannjannis111@gmail.com>
2025-02-07 17:21:58 +0100

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const MockItems = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
 const Mockdata = MockItems.map((item, index) => ({
   id: index +1,
   name: item,
 }));
-export default function HomePage() {
+export default async function HomePage() {
+  
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main>
       <div className="flex flex-col gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {Mockdata.map((Mockdata) => (
           <li key={Mockdata.id}>
             <ul>{Mockdata.name}</ul>
