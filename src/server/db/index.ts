5197ca99c2fdf83aa00d2c04f091e34a2a6344ab
Jannis Heydemann <heydemannjannis111@gmail.com>
2025-02-07 17:26:13 +0100

    fixed Database Issue

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,9 +4,9 @@ import { neon } from "@neondatabase/serverless";
 import { env } from "~/env.js";
 import * as schema from "./schema.ts";
 
-if (env.DATABASE_URL === undefined) {
-  throw new Error("DATABASE_URL must be set");
+if (env.POSTGRES_URL === undefined) {
+  throw new Error("POSTGRES_URL must be set");
 }
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.POSTGRES_URL!);
 export const db = drizzle(sql, { schema });
